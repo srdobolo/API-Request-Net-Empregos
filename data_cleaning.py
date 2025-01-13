3663ad34279d59f279fe951f5e10545e5188f63f
srdobolo <joaomiguellime@gmail.com>
2025-01-13 18:42:18 +0000

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -10,16 +10,24 @@ def clean_job_data(data, mappings):
     category = data.get('industry', {}).get('value', 'Unknown')  # Adjust according to where the category comes from
     type = data.get('employmentType', 'Unknown')  # Placeholder: Update with logic for employmentType
 
-    # Handle special cases
+    # Handle special cases on Location
     if location == "Lisbon":
         location = "Lisboa"  # Convert "Lisbon" to "Lisboa"
 
+    # Handle special cases on Category
     if category == "Customer Service":
         category = "Call Center / Help Desk"  # Convert "Customer Service" to "Call Center / Help Desk"
 
+    # Handle special cases on Type
     if type == "FULL_TIME":
-        type = "Tempo Inteiro"  # Convert "FULL_TIME" to "Tempo Inteiro"
-
+        type = "Tempo Inteiro" 
+    if type == "PART_TIME":
+        type = "Part-Time"  
+    if type == "INTERN":
+        type = "Estágio" 
+    if type == " ": # YET TO BE DEFINED
+        type = "Teletrabalho"  
+    
     # Map the category to its corresponding value in the mapping
     zona = mappings["zona_mapping"].get(location, "0")  # Default to "Unknown" if not found
     categoria = mappings["categoria_mapping"].get(category, "0")  # Default to "0" if not found
