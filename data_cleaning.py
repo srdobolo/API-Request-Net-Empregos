4e5939f6679e1a7e733d9d63d464b88801f0c7d2
srdobolo <joaomiguellime@gmail.com>
2025-01-13 19:15:39 +0000

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,59 +7,59 @@ def clean_job_data(data, mappings):
 
     # Extract values
     location = data.get('jobLocation', {}).get('address', {}).get('addressLocality', 'Foreign - Others')
-    category = data.get('industry', {}).get('value', 'Unknown')  # Adjust according to where the category comes from
-    type = data.get('employmentType', 'Unknown')  # Placeholder: Update with logic for employmentType
+    category = data.get('industry', {}).get('value', 'Unknown')  
+    type = data.get('employmentType', 'Unknown')  
 
     # Handle special cases on Location
     if location == "Lisbon":
         location = "Lisboa"  # Convert "Lisbon" to "Lisboa"
 
     # Handle special cases on Category
-        category_mappings = {
-        "Telecommunications": "Telecomunicações",
-        "Programming": "Informática ( Programação )",
-        "Industry / Production": "Indústria / Produção",
-        "Media / Communications": "Comunicação Social / Media",
-        "Hospitality / Tourism": "Hotelaria / Turismo",
-        "Education / Training": "Educação / Formação",
-        "Real Estate": "Imobiliário",
-        "Healthcare / Medicine / Nursing": "Saúde / Medicina / Enfermagem",
-        "Accounting / Finance": "Contabilidade / Finanças",
-        "Banking / Insurance / Financial Services": "Banca / Seguros / Serviços Financeiros",
-        "Public Relations": "Relações Públicas",
-        "Advertising / Marketing": "Publicidade / Marketing",
-        "Architecture / Design": "Arquitectura / Design",
-        "Civil Construction": "Construção Civil",
-        "Engineering (Mechanical)": "Engenharia ( Mecanica )",
-        "Business Management / Economics": "Gestão de Empresas / Economia",
-        "Sports / Gyms": "Desporto / Ginásios",
-        "Secretariat / Administration": "Secretariado / Administração",
-        "Shops / Commerce / Counter": "Lojas / Comércio / Balcão",
-        "HR Management": "Gestão RH",
-        "IT Training": "Informática ( Formação )",
-        "IT (Internet)": "Informática ( Internet )",
-        "IT (Multimedia)": "Informática ( Multimedia )",
-        "IT (Network Management)": "Informática ( Gestão de Redes )",
-        "IT (Systems Analysis)": "Informática ( Analise de Sistemas )",
-        "Agriculture / Forests / Fisheries": "Agricultura / Florestas / Pescas",
-        "Arts / Entertainment / Media": "Artes / Entretenimento / Media",
-        "Pharmacy / Biotechnology": "Farmácia / Biotecnologia",
-        "Restaurants / Bars / Pastry Shops": "Restauração / Bares / Pastelarias",
-        "Transport / Logistics": "Transportes / Logística",
-        "Law / Justice": "Direito / Justiça",
-        "Engineering (Civil)": "Engenharia ( Civil )",
-        "Engineering (Electrotechnical)": "Engenharia ( Eletrotecnica )",
-        "Beauty / Fashion / Wellbeing": "Beleza / Moda / Bem Estar",
-        "IT (Hardware Technician)": "Informática ( Técnico de Hardware )",
-        "Engineering (Chemical / Biological)": "Engenharia ( Química / Biologia )",
-        "Conservation / Maintenance / Technical": "Conservação / Manutenção / Técnica",
-        "Technical Services": "Serviços Técnicos",
-        "Commercial / Sales": "Comercial / Vendas",
-        "Engineering (Environmental)": "Engenharia ( Ambiente )",
-        "Social Services": "Serviços Sociais",
-        "IT (Sales/Account Manager)": "Informática (Comercial/Gestor de Conta)",
-        "Customer Service": "Call Center / Help Desk",
-        "Cleaning / Domestic": "Limpezas / Domésticas"
+    category_mappings = {
+    "Telecommunications": "Telecomunicações",
+    "Programming": "Informática ( Programação )",
+    "Industry / Production": "Indústria / Produção",
+    "Media / Communications": "Comunicação Social / Media",
+    "Hospitality / Tourism": "Hotelaria / Turismo",
+    "Education / Training": "Educação / Formação",
+    "Real Estate": "Imobiliário",
+    "Healthcare / Medicine / Nursing": "Saúde / Medicina / Enfermagem",
+    "Accounting / Finance": "Contabilidade / Finanças",
+    "Banking / Insurance / Financial Services": "Banca / Seguros / Serviços Financeiros",
+    "Public Relations": "Relações Públicas",
+    "Advertising / Marketing": "Publicidade / Marketing",
+    "Architecture / Design": "Arquitectura / Design",
+    "Civil Construction": "Construção Civil",
+    "Engineering (Mechanical)": "Engenharia ( Mecanica )",
+    "Business Management / Economics": "Gestão de Empresas / Economia",
+    "Sports / Gyms": "Desporto / Ginásios",
+    "Secretariat / Administration": "Secretariado / Administração",
+    "Shops / Commerce / Counter": "Lojas / Comércio / Balcão",
+    "HR Management": "Gestão RH",
+    "IT Training": "Informática ( Formação )",
+    "IT (Internet)": "Informática ( Internet )",
+    "IT (Multimedia)": "Informática ( Multimedia )",
+    "IT (Network Management)": "Informática ( Gestão de Redes )",
+    "IT (Systems Analysis)": "Informática ( Analise de Sistemas )",
+    "Agriculture / Forests / Fisheries": "Agricultura / Florestas / Pescas",
+    "Arts / Entertainment / Media": "Artes / Entretenimento / Media",
+    "Pharmacy / Biotechnology": "Farmácia / Biotecnologia",
+    "Restaurants / Bars / Pastry Shops": "Restauração / Bares / Pastelarias",
+    "Transport / Logistics": "Transportes / Logística",
+    "Law / Justice": "Direito / Justiça",
+    "Engineering (Civil)": "Engenharia ( Civil )",
+    "Engineering (Electrotechnical)": "Engenharia ( Eletrotecnica )",
+    "Beauty / Fashion / Wellbeing": "Beleza / Moda / Bem Estar",
+    "IT (Hardware Technician)": "Informática ( Técnico de Hardware )",
+    "Engineering (Chemical / Biological)": "Engenharia ( Química / Biologia )",
+    "Conservation / Maintenance / Technical": "Conservação / Manutenção / Técnica",
+    "Technical Services": "Serviços Técnicos",
+    "Commercial / Sales": "Comercial / Vendas",
+    "Engineering (Environmental)": "Engenharia ( Ambiente )",
+    "Social Services": "Serviços Sociais",
+    "IT (Sales/Account Manager)": "Informática (Comercial/Gestor de Conta)",
+    "Customer Service": "Call Center / Help Desk",
+    "Cleaning / Domestic": "Limpezas / Domésticas"
     }
 
     # Now you can use this mapping dynamically:
